4cf3e0ba0cc84ae666e3cf577578267c039b0cee
YanlinHuang <103290170+YanlinHuang@users.noreply.github.com>
2024-05-06 16:39:27 -0700

    Updated transformer draft

--- models/transformers.py
+++ models/transformers.py
@@ -1,1 +1,87 @@
 # Transformers
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import math
+import numpy as np
+from positional_encodings.torch_encodings import PositionalEncoding1D, Summer
+
+class TransformerModel(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(TransformerModel, self).__init__()
+        self.embedding_src = nn.Embedding(input_size, 512)
+        self.embedding_tgt = nn.Embedding(output_size, 512)
+        self.positional_encoder = PositionalEncoding(dim_model=input_size, dropout_p=0.1, max_len=5000)
+        self.transformer = nn.Transformer(d_model=512, nhead=8, num_encoder_layers=6, num_decoder_layers=6)
+        self.fc = nn.Linear(512, output_size)
+
+    def forward(self, src, tgt):
+        src = self.embedding_src(src)
+        tgt = self.embedding_tgt(tgt)
+        src = self.self.positional_encoder(src)
+        output = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=tgt_pad_mask)
+        output = self.fc(output)
+        return output
+      
+    def get_tgt_mask(self, size) -> torch.tensor:
+        mask = torch.tril(torch.ones(size, size) == 1) # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf')) # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0)) # Convert ones to 0
+        
+        # EX for size=5:
+        # [[0., -inf, -inf, -inf, -inf],
+        #  [0.,   0., -inf, -inf, -inf],
+        #  [0.,   0.,   0., -inf, -inf],
+        #  [0.,   0.,   0.,   0., -inf],
+        #  [0.,   0.,   0.,   0.,   0.]]
+        
+        return mask
+    
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+        # If matrix = [1,2,3,0,0,0] where pad_token=0, the result mask is
+        # [False, False, False, True, True, True]
+        return (matrix == pad_token)
+        
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim_model, dropout_p, max_len):
+        super().__init__()
+        # Modified version from: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+        # max_len determines how far the position can have an effect on a token (window)
+        
+        # Info
+        self.dropout = nn.Dropout(dropout_p)
+        
+        # Encoding - From formula
+        pos_encoding = torch.zeros(max_len, dim_model)
+        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1) # 0, 1, 2, 3, 4, 5
+        division_term = torch.exp(torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model) # 1000^(2i/dim_model)
+        
+        # PE(pos, 2i) = sin(pos/1000^(2i/dim_model))
+        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)
+        
+        # PE(pos, 2i + 1) = cos(pos/1000^(2i/dim_model))
+        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
+        
+        # Saving buffer (same as parameter without gradients needed)
+        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pos_encoding",pos_encoding)
+        
+    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
+        # Residual connection + pos encoding
+        return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
+
+
+# Example to train the model...(Imcompleted)
+# Prepare dataset
+input_size = 128
+output_size = 18
+
+# Model, Loss, Optimizer
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = Transformer(input_size, output_size).to(device)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
