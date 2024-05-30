3ea86c2c606fa3f601ce7f84d4c9a9eb5e5271fb
Peter Oh <chanoh5@cs.washington.edu>
2024-05-30 11:15:43 -0700

    increased epoch to overfit

--- models/linear_regresion_model2.py
+++ models/linear_regresion_model2.py
@@ -44,6 +44,27 @@ class MultiLayerPerceptron(nn.Module):
     def forward(self, x):
         return self.linear_relu_stack(x)
 
+class MultiLayerPerceptronDict(nn.Module):
+    def __init__(self, input_size, output_size, hidden_sizes=[]):
+        super(MultiLayerPerceptron, self).__init__()
+        self.layers = []
+        self.num_layers = len(hidden_sizes) + 1
+        if len(hidden_sizes) > 0:
+            self.layers.append(nn.Linear(input_size, output_size))
+        for i in range(self.num_layers):
+            if i == 0:
+                self.layers.append(nn.Linear(input_size, hidden_sizes[0]))
+            elif i == len(hidden_size):
+                self.layers.append(nn.Linear(hidden_size[i], output_size))
+            else:
+                self.layers.append(nn.Linear(hidden_size[i], hidden_size[i+1]))
+
+    def forward(self, x):
+        score = self.layers[0](x)
+        for i in range(1,self.num_layers):
+            score = self.layers[i](score)
+        return score
+
 class MultiLayerPerceptron2(nn.Module):
     def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
         super(MultiLayerPerceptron2, self).__init__()
@@ -179,8 +200,8 @@ def train(dataloader, model, loss_fn, optimizer):
             loss, current = loss.item(), (batch + 1) * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-            if isinstance(model[0], SaverModule):
-                print("first layer", model[0].saved_output)
+            # if isinstance(model[0], SaverModule):
+            #     print("first layer", model[0].saved_output)
 
     return epoch_loss
 
@@ -190,13 +211,13 @@ def test(X_tensor, y_tensor, model, loss_fn):
     with torch.no_grad():
         y_pred = model(X_tensor)
         test_loss = loss_fn(y_pred, y_tensor).item()
-        accuracies = []
+        accuracies = torch.empty(y_tensor.shape[1])
         # print("y_tensor.shape: ", y_tensor.shape)
         for i in range(y_tensor.shape[1]):
             corc = torch.corrcoef(torch.stack((y_pred[:,i], y_tensor[:,i])))
-            accuracies.append((corc[0,1]+1)/2)
-        avg_corcs = np.average(accuracies)
+            accuracies[i] = (corc[0,1]+1)/2
         print(accuracies)
+        avg_corcs = torch.mean(accuracies)
         print(f"Test Error: \n Accuracy: {(100*avg_corcs):>0.1f}%, Avg loss: {test_loss:>8f} \n")
         return test_loss, accuracies
 
@@ -340,6 +361,11 @@ scaler = StandardScaler()
 X_train_new = scaler.fit_transform(X_train_w)
 X_val = scaler.fit_transform(X_val_w)
 
+print(np.max(X_train_new))
+print(np.max(X_val))
+print(np.min(X_train_new))
+print(np.min(X_val))
+
 X_val = torch.tensor(X_val.T, dtype=torch.float32)
 y_val = torch.tensor(y_val, dtype=torch.float32)
 X = torch.tensor(X_train_new.T, dtype=torch.float32)
@@ -355,14 +381,14 @@ hidden_size1 = 80
 hidden_size2 = 30
 # model = LinearRegressionModel(X.shape[1], y.shape[1]).to(device)
 # model = MultiLayerPerceptron(X.shape[1], hidden_size1, y.shape[1]).to(device)  #
-# model = MultiLayerPerceptron2(X.shape[1], hidden_size1, hidden_size2, y.shape[1]).to(device)  #
-model = nn.Sequential(
-    SaverModule(nn.Linear(X.shape[1], hidden_size1)),
-    nn.ReLU(),
-    SaverModule(nn.Linear(hidden_size1, hidden_size2)),
-    nn.ReLU(),
-    SaverModule(nn.Linear(hidden_size2, y.shape[1])),
-).to(device)
+model = MultiLayerPerceptron2(X.shape[1], hidden_size1, hidden_size2, y.shape[1]).to(device)  #
+# model = nn.Sequential(
+#     SaverModule(nn.Linear(X.shape[1], hidden_size1)),
+#     nn.ReLU(),
+#     SaverModule(nn.Linear(hidden_size1, hidden_size2)),
+#     nn.ReLU(),
+#     SaverModule(nn.Linear(hidden_size2, y.shape[1])),
+# ).to(device)
 
 model.apply(init_weights)
 loss_fn = nn.MSELoss()
@@ -372,7 +398,7 @@ optimizer = optim.SGD(model.parameters(), lr=1e-7, momentum=0.9, nesterov=True)
 train_losses = []
 val_losses = []
 accs = []
-num_epochs = 100
+num_epochs = 500
 for epoch in range(num_epochs):
     epoch_loss = train(dataloader, model, loss_fn, optimizer)
 
@@ -402,6 +428,7 @@ plt.ylabel('Loss')
 plt.title('Losses over epoch')
 
 plt.tight_layout()
+plt.savefig('output/Losses3.png')
 plt.show()
 
 accs = np.array(accs)
@@ -415,6 +442,7 @@ plt.legend()
 plt.title('Accuracy per Channel')
 
 plt.tight_layout()
+plt.savefig('output/Accuracy3.png')
 plt.show()
 # -----------------------------------------------------------------------------
 
